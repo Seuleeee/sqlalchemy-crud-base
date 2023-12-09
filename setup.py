5474ecd449bcb29505865dd98690ccf88f062c20
Seuleeee <bryantjo1224@gmail.com>
2023-12-09 15:36:56 +0900

    Initialize Project

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="crudalchemy",
+    version="0.1.0",
+    author="Your Name",
+    description="A SQLAlchemy-based CRUD package",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Seuleeee/sqlalchemy-crud-base",
+    packages=find_packages(),
+    install_requires=[
+        "sqlalchemy>=2.0.0",
+        "psycopg2-binary>=2.8"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',  # Python 3.8 이상 필요
+)
